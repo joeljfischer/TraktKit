78aac45a83bb8e691666a72d7f3a96e8b758d2aa
Joel Fischer <joeljfischer@gmail.com>
2022-07-20 09:08:33 -0400

    Add documentation comments to TraktWatchedShow

--- Common/Models/Shows/TraktWatchedShow.swift
+++ Common/Models/Shows/TraktWatchedShow.swift
@@ -12,10 +12,19 @@ public struct TraktWatchedShow: Codable, Hashable, Identifiable {
     public var id: Int { show.id }
     
     // Extended: Min
-    public let plays: Int // Total number of plays
+    /// The total number of plays of episodes of this show
+    public let plays: Int
+
+    /// The last time a user watched a show. A user can set a custom date in the past, so this can't be used for sync.
     public let lastWatchedAt: Date?
+
+    /// The last time the show was updated. Use this to determine if a full sync of the show is needed.
     public let lastUpdatedAt: Date?
+
+    /// The show details
     public let show: TraktShow
+
+    /// Season and episode information. Use extended info noSeasons to exclude this information.
     public let seasons: [TraktWatchedSeason]?
     
     enum CodingKeys: String, CodingKey {
@@ -25,7 +34,14 @@ public struct TraktWatchedShow: Codable, Hashable, Identifiable {
         case show
         case seasons
     }
-    
+
+    /// Create a watched show for testing / preview purposes or recreating from cached data
+    /// - Parameters:
+    ///   - plays: The total number of plays of episodes of this show
+    ///   - lastWatchedAt: The last time a user watched a show. A user can set a custom date in the past, so this can't be used for sync.
+    ///   - lastUpdatedAt: The last time the show was updated. Use this to determine if a full sync of the show is needed.
+    ///   - show: The show details
+    ///   - seasons: Season and episode information. Use extended info noSeasons to exclude this information.
     public init(plays: Int, lastWatchedAt: Date?, lastUpdatedAt: Date?, show: TraktShow, seasons: [TraktWatchedSeason]?) {
         self.plays = plays
         self.lastWatchedAt = lastWatchedAt
