3e9ffc4fd22be1cf6508d169838be10cae2dfd9f
Joel Fischer <joeljfischer@gmail.com>
2022-08-18 21:12:45 -0400

    Update AccountSettings with additional properties

--- Common/Models/Users/AccountSettings.swift
+++ Common/Models/Users/AccountSettings.swift
@@ -11,6 +11,9 @@ import Foundation
 public struct AccountSettings: Codable, Hashable {
     public let user: User
     public let connections: Connections
+    public let account: Account
+    public let sharingText: SharingText
+    public let limits: Limits
     
     public struct Connections: Codable, Hashable {
         public let facebook: Bool
@@ -19,5 +22,41 @@ public struct AccountSettings: Codable, Hashable {
         public let tumblr: Bool
         public let medium: Bool
         public let slack: Bool
+    }
+
+    public struct Account: Codable, Hashable {
+        public let timeZone: String?
+        public let dateFormat: String?
+        public let time24Hr: Bool?
+        public let coverImage: URL?
+
+        enum CodingKeys: String, CodingKey {
+            case timeZone = "timezone"
+            case dateFormat = "date_format"
+            case time24Hr = "time_24hr"
+            case coverImage = "cover_image"
+        }
+    }
+
+    public struct SharingText: Codable, Hashable {
+        public let watching: String?
+        public let watched: String?
+        public let rated: String?
+    }
+
+    public struct Limits: Codable, Hashable {
+        public let list: ListLimits
+        public let watchlist: ListLimits
+        public let recommendations: ListLimits
+
+        public struct ListLimits: Codable, Hashable {
+            public let count: Int?
+            public let itemCount: Int
+
+            enum CodingKeys: String, CodingKey {
+                case count
+                case itemCount = "item_count"
+            }
+        }
     }
 }
